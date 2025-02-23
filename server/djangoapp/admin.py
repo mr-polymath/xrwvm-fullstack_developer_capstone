156903bfe7e03ca6661f6bd795fe057114289992
Your Name <you@example.com>
2025-02-23 16:08:07 -0500

    m3

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,12 +1,11 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
-# CarModelInline class
-
-# CarModelAdmin class
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # CarMakeAdmin class with CarModelInline
 
