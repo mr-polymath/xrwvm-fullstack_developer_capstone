c2c122b9ec7dc4db0fc95e3ef83576caaecce95c
Mr. Polymath <kostas.chaskos@gmail.com>
2025-03-02 18:37:18 +0200

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -15,7 +15,7 @@ def initiate():
         car_make_instances.append(
             CarMake.objects.create(name=data['name'],
                                    description=data['description']))
-    
+
     car_model_data = [{"name": "Pathfinder", "type": "SUV",
                        "year": 2023, "car_make": car_make_instances[0]},
                       {"name": "Qashqai", "type": "SUV",
